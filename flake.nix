824b1e50bb6c1762746720f3fe4dbaf67b9faa57
Will Cleghorn <deusferrariis@proton.me>
2024-06-06 20:37:08 -0600

    go dev shell flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Development shell for Go development.";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {};
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            go
+            gopls
+          ];
+        };
+      }
+    );
+}
